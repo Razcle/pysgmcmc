bf822d629ee8bf8a6a724367116d2d3baba161a9
Moritz Freidank <freidanm@informatik.uni-freiburg.de>
2017-11-15 19:39:06 +0100

    towards NUTS
    chg: first implementation that will start running. Currently we have
    issues with an endless loop that occurs.
    Furthermore, our implementation is still far from ideal:
        - we have limitations wrt. shape (and number?) of target parameters
          (only one 1d parameter supported)
        - it does not work well with our BNN (whose cost function does not
          directly map parameters to cost value)

--- pysgmcmc/samplers/nuts.py
+++ pysgmcmc/samplers/nuts.py
@@ -2,118 +2,261 @@ import tensorflow as tf
 
 from pysgmcmc.samplers.base_classes import MCMCSampler
 from pysgmcmc.tensor_utils import (
-    vectorize, unvectorize, safe_divide, safe_sqrt,
+    indicator, choice
 )
 from pysgmcmc.stepsize_schedules import ConstantStepsizeSchedule
-from pysgmcmc.tensor_utils import choice
+
+# XXX: Missing: epsilon estimation; mass adaption; supporting non-trivial shapes
+
+# XXX: We have two limitations:
+# - 1-d shapes are assumed for params in a number of places
+# - we need the cost function to map params to a tf.Tensor,
+# so this will not work with our BNN for now
 
 
 class NUTSSampler(MCMCSampler):
-    def __init__(self, params, cost_fun, batch_generator=None,
+    def __init__(self, params, cost_fun,
                  stepsize_schedule=ConstantStepsizeSchedule(0.01), seed=None,
                  session=tf.get_default_session(), dtype=tf.float64):
-        s = 1
+        self.params = params
+        self.cost_fun = cost_fun
 
-        # XXX Initialize
-        theta_minus = r_minus = epsilon = theta = j = u = r_0 = None
-        theta_plus = r_plus = None
+        self.Cost = cost_fun(self.params)
+        self.session = session
 
-        while s == 1:
+        self.delta_max = tf.constant(1.0, dtype=dtype)
+
+        # XXX: Support less plain shapes as well?
+        r_0 = tf.random_normal(shape=(), dtype=dtype)
+
+        u = tf.random_uniform(
+            shape=(),
+            minval=0.,
+            maxval=tf.exp(
+                self.cost_fun([param - 0.5 * r_0 * r_0 for param in params])
+            )
+        )
+
+        epsilon = tf.constant(0.01, dtype=dtype, name="epsilon")
+
+        # Used during while loop
+        s = tf.constant(1, dtype=tf.int32, name="s")
+        n = tf.constant(1, dtype=tf.int32, name="n")
+        j = tf.constant(0, dtype=tf.int32, name="j")
+
+        def loop_body(s, n, j, theta_last, theta_minus, r_minus,
+                      theta_plus, r_plus, r_0, u):
+            print("BODY")
             v_j = choice((-1, 1))
 
-            if v_j == -1:
-                (theta_minus, r_minus, _, _,
-                 theta_prime, n_prime, s_prime,
-                 alpha, n_alpha) = self.build_tree(
-                    theta_minus, r_minus, u, v_j, j, epsilon * theta, r_0
+            (theta_minus_tree, r_minus_tree,
+             theta_plus_tree, r_plus_tree,
+             theta_prime, n_prime, s_prime, alpha, n_alpha) = tf.cond(
+                pred=tf.equal(v_j, tf.constant(-1, dtype=v_j.dtype, name="pred_vj_const")),
+                true_fn=lambda: self.build_tree(
+                    theta_minus, r_minus,
+                    u, v_j, j, epsilon, theta_last, r_0
+                ),
+                false_fn=lambda: self.build_tree(
+                    theta_plus, r_plus,
+                    u, v_j, j, epsilon, theta_last, r_0
                 )
-            else:
-                (_, _, theta_plus, r_plus,
-                 theta_prime, n_prime, s_prime,
-                 alpha, n_alpha) = self.build_tree(
-                    theta_plus, r_plus, u, v_j, j, epsilon * theta, r_0
-                )
-
-    def _costs_for(self, param_values):
-        assignments = []
-        # XXX Cache and re-assign old parameter values?
-        for param, param_value in zip(self.params, param_values):
-            assignments.append(tf.assign(param, param_value))
-        self.session.run(assignments)
-
-        return self.session.run(self.Cost)
-
-    def leapfrog(self, theta, r, epsilon):
-        raise NotImplementedError
-
-    def build_tree(self, theta, r, u, v, j, epsilon, theta_0, r_0):
-        # XXX: Transform conditions to using tf.cond..
-
-        costs_theta_0 = self._costs_for(theta_0)
-        if j == 0:
-            theta_prime, r_prime = self.leapfrog(theta, r, v * epsilon)
-
-            costs_theta_prime = self._costs_for(theta_prime)
-
-            n_prime = tf.cast(
-                tf.less_equal(
-                    u,
-                    tf.exp(costs_theta_prime - 0.5 * r_prime * r_prime)
-                ), tf.int32
             )
 
-            s_prime = tf.cast(
+            assign_ops = tf.cond(
+                pred=tf.equal(v_j, tf.constant(-1, dtype=v_j.dtype, name="pred2_vj_const")),
+                true_fn=lambda: [tf.assign(theta_minus, theta_minus_tree),
+                                 tf.assign(r_minus, r_minus_tree)],
+                false_fn=lambda: [tf.assign(theta_plus, theta_plus_tree),
+                                  tf.assign(r_plus, r_plus_tree)],
+            )
+
+            with tf.control_dependencies(assign_ops):
+                probability_bound = tf.minimum(
+                    tf.constant(1., dtype=tf.float64, name="probbound_1_const"),
+                    tf.divide(n_prime, n)
+                )
+
+                theta_m = tf.cond(
+                    pred=tf.logical_and(
+                        tf.equal(s_prime, tf.constant(1, dtype=s_prime.dtype)),
+                        tf.less_equal(
+                            tf.random_uniform(shape=(), maxval=1., dtype=tf.float64),
+                            probability_bound
+                        )
+                    ),
+                    true_fn=lambda: [tf.assign(theta_val, theta_prime_val) for theta_val, theta_prime_val in zip(params, theta_prime)],
+                    false_fn=lambda: theta_last
+                )
+
+                s_new = tf.multiply(
+                    s_prime * indicator(
+                        tf.greater_equal((theta_plus - theta_minus) * r_minus, 0),
+                    ),
+                    indicator(
+                        tf.greater_equal((theta_plus - theta_minus) * r_plus, 0)
+                    )
+                )
+
+                return (
+                    s_new, n + n_prime, j + 1,
+                    theta_m,
+                    theta_minus, r_minus,
+                    theta_plus, r_plus,
+                    r_0, u
+                )
+
+        a, *_ = tf.while_loop(
+            cond=lambda s, *_: tf.equal(s, tf.constant(1, dtype=s.dtype)),
+            body=loop_body,
+            loop_vars=[s, n, j, params, params, r_0, params, r_0, r_0, u]
+        )
+        session.run(a)
+
+        # XXX: Mass adaptation to estimate epsilon
+
+    def leapfrog(self, theta, r, epsilon):
+        # XXX: Currently limited to 1d values!
+        old_costs = self.cost_fun(theta)
+        old_gradient = tf.gradients(old_costs, theta)[0]
+        r_bar = r + tf.divide(epsilon, tf.constant(2., dtype=epsilon.dtype)) * old_gradient
+
+        theta_bar = theta + epsilon * r_bar
+        new_costs = self.cost_fun(theta_bar)
+        new_gradient = tf.gradients(new_costs, theta_bar)[0]
+        r_bar = r_bar + tf.divide(epsilon, tf.constant(2., dtype=epsilon.dtype)) * new_gradient
+        return theta_bar, r_bar
+
+    def build_tree(self, theta, r, u, v, j, epsilon, theta_0, r_0):
+        costs_theta_0 = self.cost_fun(theta_0)
+
+        def base_case(theta, r, v, epsilon, u, r_0, costs_theta_0):
+            theta_prime, r_prime = self.leapfrog(theta=theta, r=r, epsilon=tf.cast(v, dtype=epsilon.dtype) * epsilon)
+
+            costs_theta_prime = self.cost_fun(theta_prime)
+
+            n_prime = indicator(
                 tf.less_equal(
+                    u, tf.exp(costs_theta_prime - 0.5 * r_prime * r_prime)
+                )
+            )
+
+            s_prime = indicator(
+                tf.less(
                     u,
-                    self.delta_max + costs_theta_prime - 0.5 * r_prime * r_prime
-                ), tf.int32
+                    tf.exp(self.delta_max + costs_theta_prime - 0.5 * r_prime * r_prime)
+                )
+            )
+
+            minimum_term = tf.minimum(
+                tf.constant(1., dtype=costs_theta_prime.dtype, name="probbound2_const"),
+                tf.exp(
+                    costs_theta_prime - 0.5 * r_prime *
+                    r_prime - costs_theta_0 + 0.5 * r_0 * r_0
+                )
             )
 
             return (
-                theta_prime, r_prime, theta_prime, r_prime, theta_prime,
-                n_prime, s_prime,
-                tf.minimum(
-                    1,
-                    tf.exp(costs_theta_prime - 0.5 * r_prime * r_prime - costs_theta_0 + 0.5 * r_0 * r_0)
-                ),
-                1
+                theta_prime, r_prime, theta_prime, r_prime,
+                theta_prime, n_prime, s_prime, minimum_term, 1
             )
-        else:
-            tree_results = self.build_tree(
+
+        def recursion(theta, r, u, v, j, epsilon, theta_0, r_0):
+            (theta_minus, r_minus, theta_plus, r_plus,
+             theta_prime, n_prime, s_prime,
+             alpha_prime, n_alpha_prime) = self.build_tree(
                 theta, r, u, v, j - 1, epsilon, theta_0, r_0
             )
 
-            (theta_minus, r_minus, theta_plus,
-             r_plus, theta_prime, n_prime,
-             s_prime, alpha_prime, n_alpha_prime) = tree_results
-
-            if s_prime == 1:
-                if v == -1:
-                    (theta_minus, r_minus, _, _,
-                     theta_prime_prime, n_prime_prime,
-                     s_prime_prime, alpha_prime_prime,
-                     n_alpha_prime_prime) = self.build_tree(
+            def true_fn_sprime_equals_1(theta_minus, r_minus,
+                                        theta_plus, r_plus,
+                                        u, v, j, epsilon, theta_0, r_0,
+                                        alpha_prime, n_alpha_prime, n_prime):
+                (theta_minus_tree, r_minus_tree,
+                 theta_plus_tree, r_plus_tree,
+                 theta_prime_prime, n_prime_prime,
+                 s_prime_prime, alpha_prime_prime,
+                 n_alpha_prime_prime) = tf.cond(
+                    pred=tf.equal(v, tf.constant(-1, dtype=v.dtype, name="pred1_v_const")),
+                    true_fn=lambda: self.build_tree(
                         theta_minus, r_minus, u, v, j - 1,
                         epsilon, theta_0, r_0
-                    )
-                else:
-                    (_, _, theta_plus, r_plus,
-                     theta_prime_prime, n_prime_prime,
-                     s_prime_prime, alpha_prime_prime,
-                     n_alpha_prime_prime) = self.build_tree(
+                    ),
+                    false_fn=lambda: self.build_tree(
                         theta_plus, r_plus, u, v, j - 1,
                         epsilon, theta_0, r_0
                     )
+                )
 
-                # XXX: With probability n'' / (n' + n'')
+                theta_minus, r_minus = tf.cond(
+                    pred=tf.equal(v, tf.constant(-1., dtype=v.dtype, name="pred2_v_const")),
+                    true_fn=lambda: (theta_minus_tree, r_minus_tree),
+                    false_fn=lambda: (theta_minus, r_minus)
+                )
+                theta_plus, r_plus = tf.cond(
+                    pred=tf.equal(v, tf.constant(-1., dtype=v.dtype, name="pred3_v_const")),
+                    true_fn=lambda: (theta_plus, r_plus),
+                    false_fn=lambda: (theta_plus_tree, r_plus_tree),
+                )
 
+                theta_prime = tf.cond(
+                    pred=tf.less_equal(
+                        tf.random_uniform(shape=(), maxval=0., dtype=n_prime_prime.dtype),
+                        tf.divide(n_prime_prime, n_prime + n_prime_prime),
+                    ),
+                    true_fn=lambda: theta_prime_prime,
+                    false_fn=lambda: theta_prime
+                )
                 alpha_prime = alpha_prime + alpha_prime_prime
                 n_alpha_prime = n_alpha_prime + n_alpha_prime_prime
                 n_prime = n_prime + n_prime_prime
 
-                return (
-                    theta_prime, r_prime,
-                    theta_plus, r_plus,
-                    theta_prime, n_prime,
-                    s_prime, alpha_prime, n_alpha_prime
+                s_prime = tf.multiply(
+                    s_prime_prime * indicator(
+                        tf.greater_equal(
+                            (theta_plus - theta_minus) * r_minus,
+                            tf.constant(0., dtype=theta_plus.dtype, name="gteq_const_0")
+                        )
+                    ), indicator(
+                        tf.greater_equal(
+                            (theta_plus - theta_minus) * r_plus,
+                            tf.constant(0., dtype=theta_plus.dtype, name="gteq_const_1"))
+                    )
                 )
+
+                return (
+                    theta_minus, r_minus, theta_plus, r_plus,
+                    theta_prime, n_prime, s_prime,
+                    alpha_prime, n_alpha_prime
+                )
+
+            theta_minus, r_minus, theta_plus, r_plus,
+            theta_prime, n_prime, s_prime,
+            alpha_prime, n_alpha_prime = tf.cond(
+                pred=lambda: tf.equal(s_prime, tf.constant(1., dtype=s_prime.dtype, name="s_prime_const")),
+                true_fn=lambda: true_fn_sprime_equals_1(
+                    theta_minus, r_minus,
+                    theta_plus, r_plus,
+                    u, v, j, epsilon, theta_0, r_0,
+                    alpha_prime, n_alpha_prime, n_prime
+                ),
+                false_fn=lambda: (
+                    theta_minus, r_minus, theta_plus, r_plus,
+                    theta_prime, n_prime, s_prime,
+                    alpha_prime, n_alpha_prime
+                )
+            )
+            return (
+                theta_minus, r_minus, theta_plus, r_plus, theta_prime, n_prime,
+                s_prime, alpha_prime, n_alpha_prime
+            )
+        (theta_minus, r_minus, theta_plus, r_plus, theta_prime, n_prime,
+         s_prime, alpha_prime, n_alpha_prime) = tf.cond(
+            pred=tf.equal(j, tf.constant(0, dtype=j.dtype, name="0_const0")),
+            true_fn=lambda: base_case(theta, r, v, epsilon, u, r_0, costs_theta_0),
+            false_fn=lambda: recursion(theta, r, u, v, j, epsilon, theta_0, r_0)
+        )
+        return (
+            theta_minus, r_minus, theta_plus, r_plus, theta_prime, n_prime,
+            s_prime, alpha_prime, n_alpha_prime
+        )
